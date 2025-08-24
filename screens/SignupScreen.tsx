14681522b8a77f130c61955dc3e8601dbc9cc8f3
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-24 17:23:01 +0330

    feat(auth): add loading state to `SignupScreen`
    Introduced a loading state and `LoadingOverlay` component to enhance user experience during the sign-up process. Modified `SignupScreen` to include loading logic and pass `signupHandler` to `AuthContent`.

--- screens/SignupScreen.tsx
+++ screens/SignupScreen.tsx
@@ -1,7 +1,26 @@
 import AuthContent from '../components/auth/AuthContent';
+import {createUser} from "../services/auth";
+import {useState} from "react";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [loading, setLoading] = useState(false);
+  async function signupHandler({email, password}: { email: string, password: string }) {
+    setLoading(true)
+    try {
+      await createUser(email, password)
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  if (loading) {
+    return <LoadingOverlay message="Creating user..." />
+  }
+
+  return <AuthContent isLogin={false} onAuthenticate={signupHandler}/>;
 }
 
 export default SignupScreen;
