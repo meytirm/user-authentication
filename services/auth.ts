4545e5a6e568a91b14481079c9251d8d1d9259e2
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-24 17:22:19 +0330

    refactor(auth): simplify `createUser` function
    Removed unnecessary `async/await` in the `createUser` function for enhanced simplicity and clarity.

--- services/auth.ts
+++ services/auth.ts
@@ -1,7 +1,7 @@
 import {fetch} from "expo/fetch";
 
-export async function createUser(email: string, password: string) {
-  const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=REMOVED_API_KEY', {
+export function createUser(email: string, password: string) {
+  return fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=REMOVED_API_KEY', {
     method: 'POST',
     body: JSON.stringify({
       email,
