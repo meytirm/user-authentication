fa68c61e1f5240d8a8535219f465c530fecc9507
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-25 12:50:24 +0330

    feat(auth): add loading state to `LoginScreen`
    Introduced a loading state and `LoadingOverlay` component to enhance user experience during the login process. Updated `LoginScreen` to include loading logic and pass `loginHandler` to `AuthContent`. Added error handling for failed authentication attempts.

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,7 +1,27 @@
 import AuthContent from '../components/auth/AuthContent';
+import {useState} from "react";
+import {loginUser} from "../services/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import {Alert} from "react-native";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [loading, setLoading] = useState(false);
+  async function loginHandler({email, password}: { email: string, password: string }) {
+    setLoading(true)
+    try {
+      await loginUser(email, password)
+    } catch (e) {
+      Alert.alert('Authentication failed!', 'could not login. please check your credentials')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  if (loading) {
+    return <LoadingOverlay message="Logging in..." />
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
