15d9e0188c76c4a3f565428800bc19e2af8e4084
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-26 20:14:20 +0330

    fix(auth): add explicit `AuthResponse` type cast to authentication methods
    Ensured `createUser` and `loginUser` return promises with `AuthResponse` type by adding explicit type casting. This improves type safety and consistency in authentication service.

--- services/auth.ts
+++ services/auth.ts
@@ -1,13 +1,14 @@
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
 import {firebaseAuth} from "./firebase";
+import {AuthResponse} from "../types/auth";
 
 
 
 export function createUser(email: string, password: string) {
-  return createUserWithEmailAndPassword(firebaseAuth, email, password)
+  return createUserWithEmailAndPassword(firebaseAuth, email, password) as unknown as Promise<AuthResponse>
 }
 
 export function loginUser(email: string, password: string) {
-  return signInWithEmailAndPassword(firebaseAuth, email, password)
+  return signInWithEmailAndPassword(firebaseAuth, email, password) as unknown as Promise<AuthResponse>
 }
 
