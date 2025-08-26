58508f21c0a2148565ea127210cd7e2f463c831d
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-26 20:13:31 +0330

    feat(auth): integrate token-based authentication in `LoginScreen`
    Added `token` retrieval and authentication using `AuthContext` in `loginHandler`. Enhanced error handling for missing `AuthContext`. This improves user authentication flow and ensures state consistency.

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,15 +1,22 @@
 import AuthContent from '../components/auth/AuthContent';
-import {useState} from "react";
+import {useContext, useState} from "react";
 import {loginUser} from "../services/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import {Alert} from "react-native";
+import {AuthContext} from "../store/auth-context";
 
 function LoginScreen() {
   const [loading, setLoading] = useState(false);
+  const authContext = useContext(AuthContext)
   async function loginHandler({email, password}: { email: string, password: string }) {
+    if (!authContext) {
+      throw new Error('AuthContext not found')
+    }
     setLoading(true)
     try {
-      await loginUser(email, password)
+      const response = await loginUser(email, password)
+      const token = response._tokenResponse.idToken
+      authContext.authenticate(token)
     } catch (e) {
       Alert.alert('Authentication failed!', 'could not login. please check your credentials')
     } finally {
